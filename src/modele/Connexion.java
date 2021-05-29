1b2b453143ceda6f9742e242df03f5e3fcfd958a
Jules Delamare <julesd2509@gmail.com>
2021-05-29 12:51:17 +0200

    Ajout d'une classe de constante pour la connexion et d'une methode pour fermer la connexion

--- src/modele/Connexion.java
+++ src/modele/Connexion.java
@@ -8,33 +8,37 @@ import java.sql.Statement;
 
 public class Connexion {
 	static Connection conn;
-	
+
 	public Connexion(String addresse, String username, String password) throws SQLException, ClassNotFoundException{
 		/*
 		DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
 		conn = DriverManager.getConnection ("jdbc:oracle:thin:@madere:1521:info", username, password);
-		*/
+		 */
 		conn = DriverManager.getConnection(addresse);
-    }
-	
+	}
+
 	public static ResultSet executeQuery(String query) throws SQLException {
 		try {
 			Statement stmt = conn.createStatement();
-	    	ResultSet rset = stmt.executeQuery(query);
-	    	return rset;
+			ResultSet rset = stmt.executeQuery(query);
+			return rset;
 		} catch (SQLException e) {
 			System.out.println("Erreur dans l'execution de la requete: " + e.toString());
 		}
 		return null;
 	}
-	
+
 	public static void executeUpdate(String sql) throws SQLException {
 		try {
 			Statement stmt = conn.createStatement();
-	    	stmt.executeUpdate(sql);
+			stmt.executeUpdate(sql);
 		} catch (SQLException e) {
 			System.out.println("Erreur dans l'execution de la requete: '" + sql + "' \n" + e.toString());
 		}
+	}
+	
+	public static void closeConnexion(){
+		 conn = null;
 	}
 	
 	public static void executeUpdate(String sql, String[] params) throws SQLException {
